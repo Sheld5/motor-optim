7cbe8f8fa586bb4937013c09936de7f4abb00b19
Sheld5 <simonsoldat@seznam.cz>
2023-10-11 16:24:57 +0200

    add grid.jl

---
+++ grid.jl
@@ -0,0 +1,61 @@
+using BOSS
+using Random
+include("Surrogate_Q_volne_parametry.jl")
+
+Random.seed!(555)
+
+function get_domain()
+    constraints(x) = [ModelParam.check_feas(x...)...]
+    return BOSS.Domain(;
+        bounds = ModelParam.domain(),
+        discrete = ModelParam.discrete_dims(),
+        cons = constraints,
+    )
+end
+
+function grid(; save=false)
+    lhc_size = 120
+    bounds = ModelParam.domain()
+    domain = get_domain()
+
+    # generate X
+    X = BOSS.generate_starts_LHC(bounds, lhc_size)
+    for dim in 1:length(ModelParam.discrete_dims())
+        ModelParam.discrete_dims()[dim] && (X[dim,:] .= round.(X[dim,:]))
+    end
+
+    # exclude exterior points
+    X = reduce(hcat, (x for x in eachcol(X) if BOSS.in_domain(x, domain)))
+
+    # calculate Y
+    # Y = reduce(hcat, (ModelParam.calc(x...) for x in eachcol(X)))
+    
+    println("$(size(X)[2]) samples")
+    save && save_grid(X)
+    return X
+end
+
+function save_grid(X)
+    open("grid.txt", "w+") do io
+        
+        println(io)
+        println(io, "\tnk\tdk\tDs\tQ")
+        for x in eachcol(X)
+            for n in x
+                print(io, ' ', n)
+            end
+            println(io)
+        end
+
+        # println(io)
+        # println(io, "\tdP\tTav")
+        # for y in eachcol(Y)
+        #     for n in y
+        #         print(io, ' ', n)
+        #     end
+        #     println(io)
+        # end
+
+        println()
+    end
+end
